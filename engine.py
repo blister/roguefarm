ea860909608b0128694cb36cf7c82d996436a9dc
Eric Harrison <github@hrrsn.co>
2022-01-29 23:33:03 -0500

    Progress/leveling system

--- engine.py
+++ engine.py
@@ -10,9 +10,9 @@ from tcod.map import compute_fov
 
 import exceptions
 from entity import Entity
-from game_map import GameMap
+from game_map import GameMap, GameWorld
 from message_log import MessageLog
-from render_functions import render_bar, render_names_at_mouse_location
+import render_functions
 
 if TYPE_CHECKING:
 	from entity import Actor
@@ -21,6 +21,7 @@ if TYPE_CHECKING:
 class Engine:
 
 	game_map: GameMap
+	game_world: GameWorld
 		
 	def __init__(self, player: Actor):
 		self.message_log = MessageLog()
@@ -58,12 +59,22 @@ class Engine:
 
 		self.message_log.render(console=console, x=21, y=45, width=40, height=5)
 
-		render_bar(
+		render_functions.render_bar(
 			console=console,
 			current_value=self.player.fighter.hp,
 			maximum_value=self.player.fighter.max_hp,
 			total_width=20
 		)
 
-		render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+		render_functions.render_dungeon_level(
+			console=console,
+			dungeon_level=self.game_world.current_floor,
+			location=(0,47)
+		)
+
+		render_functions.render_names_at_mouse_location(
+			console=console, 
+			x=21, y=44, 
+			engine=self
+		)
 
